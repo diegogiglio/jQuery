83df916c74043314929d2b1ab48a74378a45bdc4
Diego Giglio <diegoagiglio@gmail.com>
2019-08-28 20:50:18 -0300

    show and hide elents

--- 07-01-show-hide-toggle/js/script.js
+++ 07-01-show-hide-toggle/js/script.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
-	$('.item-box').on('click', 'a.info-link', function(event){
+	/*$('.item-box').on('click', 'a.info-link', function(event){
 		event.preventDefault();
 
 		//hide(), toggle()
 		$(this).closest('.item-box').find('.more-info').toggle('slow', function(){
 			alert('finished');
 		});
-	});
+	});*/
+
+	$(".item-box").on('click', 'a.info-link', function(event){
+		event.preventDefault();
+
+		$(this).closest('.item-box').find('.more-info').
+		//toggle('slow', function(){alert('Finished!')});
+		//fadeIn('fast');
+		//fadeToggle('always', function(){alert('oi')});
+		slideToggle('slow');
+		
+	})
+
 });
