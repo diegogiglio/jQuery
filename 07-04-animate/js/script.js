e83fc330b935c6e2da36a26141e4362c7d760d7f
Diego Giglio <diegoagiglio@gmail.com>
2019-08-28 20:50:58 -0300

    animate elements manipulating css

--- 07-04-animate/js/script.js
+++ 07-04-animate/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#example1').on('click', function(){
+	/*$('#example1').on('click', function(){
 		$(this).animate({
 			opacity: 0.3
 		}, 500, function(){
@@ -19,5 +19,35 @@ $(document).ready(function() {
 			left: '+=20',
 			width: '+=50'
 		}, 500);
-	});
+	});*/
+
+	$('#example1').on('click', function(){
+		$(this).animate({
+			opacity: '-=0.1'
+		}, 500, function(){
+			alert('completed!')
+		});
+	})
+
+
+	$('#example2').on('click', function(){
+		$(this).animate({
+			left: 200,
+			width: 40
+		}, 500, function(){
+			alert('completed!')
+		});
+	})
+
+
+	$('#example3').on('click', function(){
+		$(this).animate({
+			left: '+=20',
+			width: '+=50'
+		}, 500, function(){
+			alert('completed!')
+		});	
+	})
+
+
 });
